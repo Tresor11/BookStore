b47b66464d89d456c38c0797fece3480cb303e7c
Selma Datagirl <datagirlcmr@gmail.com>
2020-05-21 13:59:45 -0500

    styled the add book form

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -31,13 +31,14 @@ class BooksForm extends React.Component {
     const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
     return (
       <div>
-        <form>
-          <input type="text" required placeholder="Book title" name="title" value={this.state.title} onChange={this.handleChange} />
-          <select name="category" value={this.state.category} onChange={this.handleChange}>
-            <option value="">select category</option>
+        <h5 class="form-control">ADD NEW BOOK</h5>
+        <form className="form-control">
+          <input type="text" className="w-50 h-40" required placeholder="Book title" name="title" value={this.state.title} onChange={this.handleChange} />
+          <select name="category" className="w-25 h-40" value={this.state.category} onChange={this.handleChange}>
+            <option value="">Category</option>
             {categories.map(el => <option key={Math.random() * 100} value={el}>{el}</option>)}
           </select>
-          <button type="submit" onClick={this.handleSubmit}>Submit</button>
+          <button type="submit" className="w-20 h-40" onClick={this.handleSubmit}>Add Book</button>
         </form>
       </div>
     );
