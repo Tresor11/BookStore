f8873bf8d8250e36ece5ba2e9fc9d66dc9102c98
Tresor11 <tresor.moise2001@gmail.com>
2020-05-21 14:00:17 +0200

    added the default option for category filter

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -2,11 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const CategoryFilter = ({ onChange, value }) => {
-  const categories = ['ALL', 'Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+  const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
   return (
     <div>
       <p>Category</p>
       <select onChange={onChange} value={value}>
+        <option value="ALL">All</option>
         {categories.map(el => <option key={Math.random() * 100} value={el}>{el}</option>)}
       </select>
     </div>
