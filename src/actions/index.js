fbd972d204b121cf0026ba4521f5862e795e0230
Tresor11 <tresor.moise2001@gmail.com>
2020-05-25 21:34:20 +0200

    removed the unari operator for a single line

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,8 @@
-/* eslint-disable no-plusplus */
 let id = 4;
 
 const CREATE_BOOK = book => ({
   type: 'CREATE_BOOK',
+  // eslint-disable-next-line no-plusplus
   id: id++,
   title: book.title,
   category: book.category,
